5dc421f37d89e81fd6b581df9eeec7dd372fa901
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-10 15:06:15 -0500

    Static tiles added. Now I have to use vector tiles to make the app better. Although a lot of stuff is left to do yet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,6 +8,15 @@ class Map extends React.Component{
 
     componentDidMount(){
         var mymap = L.map('map').setView([`${this.props.lat}`, `${this.props.lon}`], 13);
+
+        L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/512/1/1/0@2x?access_token=pk.eyJ1IjoiaXZqYyIsImEiOiJja3M2Z2ljcTIxNzBpMnByejZsaTFiOWdjIn0.qchk1ClOLnbqLwdLpOzSCg", {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(mymap);
     }
 
     render(){
