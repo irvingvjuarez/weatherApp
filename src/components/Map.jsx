16501d47b569a0fa2f228e8e37aa26716defd5dc
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-10 18:14:43 -0500

    The map is already rendering in the correct coordinates. Documentation is left to read to understand how the map works

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,16 +7,13 @@ class Map extends React.Component{
     }
 
     componentDidMount(){
-        var mymap = L.map('map').setView([`${this.props.lat}`, `${this.props.lon}`], 13);
-
-        L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/512/1/1/0@2x?access_token=pk.eyJ1IjoiaXZqYyIsImEiOiJja3M2Z2ljcTIxNzBpMnByejZsaTFiOWdjIn0.qchk1ClOLnbqLwdLpOzSCg", {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'your.mapbox.access.token'
-        }).addTo(mymap);
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaXZqYyIsImEiOiJja3M2Z2ljcTIxNzBpMnByejZsaTFiOWdjIn0.qchk1ClOLnbqLwdLpOzSCg';
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [`${this.props.lon}`, `${this.props.lat}`], // starting position [lng, lat]
+            zoom: 5 // starting zoom
+        });
     }
 
     render(){
