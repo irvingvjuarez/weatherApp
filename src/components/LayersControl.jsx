9ca1e8eefaa1d23f3f5cc7055873945d152f6d85
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-11 21:48:03 -0500

    The button to hide the layers menu kinda work, but there are things to be solved yet. I am going to go forward to the layers right now

--- src/components/LayersControl.jsx
+++ src/components/LayersControl.jsx
@@ -1,5 +1,7 @@
 import "./styles/LayersControl.css"
 
+var xCoord = []
+
 class MapboxGLButtonControl {
     constructor({
         className = "",
@@ -13,12 +15,12 @@ class MapboxGLButtonControl {
 
     onAdd(map) {
         this._btn = document.createElement("button");
-        this._btn.className = "layer-control-container"
+        this._btn.className = this._className
         this._btn.type = "button";
         this._btn.title = this._title;
-        this._btn.onclick = this._eventHandler;
 
         this._img = document.createElement("img")
+        this._img.onclick = this._eventHandler;
         this._img.src="../assets/icons/layers.svg"
         this._img.classList.add("button-img")
 
@@ -34,11 +36,72 @@ class MapboxGLButtonControl {
 }
 
 function clickHandler(e){
-    console.log(e)
+    let container = e.target.parentNode
+    container.classList.add("layer-menu")
+    container.innerHTML = `
+        <ul class="list">
+            <li>Temperature</li>
+            <li>Wind speed</li>
+            <li>Clouds</li>
+            <li>Precipitation</li>
+            <li>None</li>
+        </ul>
+        <div class="close-button">
+            <div class="close-button-img"></div>
+        </div>
+    `
+
+    let closeButton = document.querySelector(".close-button")
+    closeButton.addEventListener("touchmove", handleMovement)
+    closeButton.addEventListener("touchend", endTouch)
+    closeButton.addEventListener("mousedown", handleMouseMovement)
+    closeButton.addEventListener("mouseup", endTouch)
+}
+
+const endMouseMovemement = (e) => {
+    e.target.removeEventListener("mousemove", handleMovement)
+    endTouch(e)
+}
+
+const handleMouseMovement = (e) => {
+    e.target.addEventListener("mousemove", handleMovement)
+}
+
+const handleMovement = (e) => {
+    let xcoor = e.screenX || e.changedTouches[0].screenX
+    xCoord.push(xcoor)
+
+    if(xCoord.length > 0){
+        let index = xCoord.length - 1
+
+        if(xCoord[index] > xCoord[index - 1]){
+            if(index < 35){
+                e.target.style.marginLeft = `${index}px`
+            }
+        }
+    }
+}
+
+const endTouch = (e) => {
+    let container = e.target.parentNode.parentNode
+    xCoord = []
+    e.target.style.marginLeft = "0px"
+    let children = [...container.children]
+
+    setTimeout(() => {
+        container.classList.remove("layer-menu")
+        children.map(item => {
+            item.style.display = "none"
+        })
+        // originalButton.style.zIndex = "1"
+        container.classList.add("layer-control-container")
+    }, 150)
+
+    e.target.removeEventListener("mousemove", handleMovement)
 }
 
 const LayersControl = new MapboxGLButtonControl({
-    className: "",
+    className: "layer-control-container",
     title: "See layers",
     eventHandler: clickHandler
 });
