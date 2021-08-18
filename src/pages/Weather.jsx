b28feeeeebf9bd12a91ad10dec64c33511e01c55
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-18 09:52:31 -0500

    images added

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -3,6 +3,8 @@ import Section from "../components/Section"
 import BasicInfo from "../components/BasicInfo"
 import TempOverview from "../components/TempOverview"
 import Carousel from "../components/Carousel"
+import DetailCard from "../components/DetailCard"
+import WeatherColumn from "../components/WeatherColumn"
 
 import "./styles/Weather.css"
 
@@ -15,9 +17,34 @@ class Weather extends React.Component{
         return(
             <React.Fragment>
                 <BasicInfo />
+
                 <TempOverview />
+
                 <Section title="Temperature over time">
                     <Carousel />
+                </Section>
+
+                <Section title="Details">
+                    <section className="details-container">
+                        <DetailCard title="Wind" imgUrl="../assets/icons/wind.png"/>
+                        <DetailCard title="Humidity" imgUrl="../assets/icons/humidity.png"/>
+                        <DetailCard title="Pressure" imgUrl="../assets/icons/gauge.png"/>
+                    </section>
+                </Section>
+
+                <Section title="Rain probability">
+                    <Carousel />
+                </Section>
+
+                <Section title="Next 7 days">
+                    <section className="nextDaysWeather">
+                        <WeatherColumn />
+                        <WeatherColumn />
+                        <WeatherColumn />
+                        <WeatherColumn />
+                        <WeatherColumn />
+                        <WeatherColumn />
+                    </section>
                 </Section>
             </React.Fragment>
         )
