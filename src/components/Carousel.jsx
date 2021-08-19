4dfc049627891c06aac3ff4c3144e4f3306a3170
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-18 19:19:31 -0500

    Charts added, but there are problems with the design. Working on it right now

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,21 @@
 import React from "react"
+import {
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip
+} from "recharts";
 import "./styles/Carousel.css"
+
+const data = [
+    {time: "Now", temp: "15.3"},
+    {time: "1pm", temp: "14.2"},
+    {time: "2pm", temp: "20.3"},
+    {time: "3pm", temp: "15"},
+    {time: "4pm", temp: "14.5"}
+]
 
 class Carousel extends React.Component{
     constructor(props){
@@ -8,7 +24,20 @@ class Carousel extends React.Component{
 
     render(){
         return(
-            <canvas id="chart"></canvas>
+            <LineChart
+                width={300}
+                height={113}
+                data={data}
+                margin={{ top: 5, right: 0, bottom: 5, left: 0 }}
+            >
+
+                <Line type="monotone" dataKey="degrees" stroke="#8884d8" />
+                <CartesianGrid stroke="#fff" />
+                <XAxis dataKey="time" />
+                <YAxis type="number" name="temp" unit="°"/>
+                <Tooltip />
+
+            </LineChart>
         )
     }
 }
