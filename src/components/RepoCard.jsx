c27d81d3101db171f9d23ecb8dbbb8241417ab7b
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-22 12:16:28 -0500

    The Contact page done

---
+++ src/components/RepoCard.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import repoLogo from "../assets/icons/bookLogo.png"
+import "./styles/RepoCard.css"
+
+const RepoCard = (props) => {
+    return(
+        <div className="repo-main">
+            <div className="repo-main__header">
+                <img src={repoLogo} alt="repo logo" />
+                <a href={props.link}>{props.title}</a>
+            </div>
+
+            <div className="repo-main__body">
+                {props.description}
+            </div>
+        </div>
+    )
+}
+
+export default RepoCard
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from "react"
-import Loader from "../components/Loader"
 import Section from "../components/Section"
+import RepoCard from "../components/RepoCard"
 
 import avatar from "../assets/images/avatar3.jpg"
+import twitter from "../assets/icons/twitter.png"
+import github from "../assets/icons/github.png"
+import mail from "../assets/icons/mail.png"
 
 import "./styles/Contact.css"
 
@@ -10,77 +13,90 @@ class Contact extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            loading: false,
-            error: null
+            data: null
         }
     }
 
     componentDidMount(){
-        // this.fetchGitHubData()
+        this.fetchGitHubData()
     }
 
     fetchGitHubData = async() => {
-        this.setState({
-            loading: true,
-            error: null
-        })
         const API = "https://api.github.com/users/IrvingJuarez/repos"
 
         try{
             const response = await fetch(API)
             const data = await response.json()
-            return data
-        }catch(error){
+
             this.setState({
-                loading: false,
-                error: error
+                data: data
             })
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    renderRepoData = () => {
+
+        if(this.state.data){
+            return(
+                <Section title="Latest repositories">
+                    <article className="repos-container">
+                        {this.state.data.map(repo => {
+                            let flag = false
+                            if(repo.name == "Pacman-Remastered"){
+                                flag = true
+                            }else if(repo.name == "equation-solver"){
+                                flag = true
+                            }else if(repo.name == "weatherApp"){
+                                flag = true
+                            }
+        
+                            if(flag){
+                                console.log(repo)
+                                return(
+                                    <RepoCard key={repo.id} title={repo.name} link={repo.url} description={repo.description} />
+                                )
+                            }
+                        })}
+                    </article>
+                </Section>
+            )
         }
     }
 
     render(){
-        if(this.state.loading){
-            return(
-                <section className="contact-main">
-                    <Loader />
-                </section>
-            )
-        }else if(this.state.error){
-            return(
-                <section className="contact-main">
+        return(
+            <section className="contact-main">
+                <Section title="">
+                    <img src={avatar} className="avatarImage" />
+                    <div className="titles">
+                        <h2>Irving Juárez</h2>
+                        <h3>Fronted developer</h3>
+                        <nav className="socialMedia">
+                            <ul>
+                                <li><a href="https://twitter.com/juarez1_irving"><img src={twitter} /></a></li>
+                                <li><a href="https://github.com/IrvingJuarez/"><img src={github} /></a></li>
+                                <li><a href="mailto: irvingjuarez274@gmail.com"><img src={mail} /></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </Section>
 
-                </section>
-            )
-        }else{
-            return(
-                <section className="contact-main">
-                    <Section title="">
-                        <img src={avatar} className="avatarImage" />
-                        <div className="titles">
-                            <h2>Irving Juárez</h2>
-                            <h3>Fronted developer</h3>
-                            <nav className="socialMedia">
-                                <ul>
-                                    <li><a href="https://twitter.com/juarez1_irving"><img src="" /></a></li>
-                                    <li><a href="https://github.com/IrvingJuarez/"><img src="" /></a></li>
-                                    <li><a href="mailto: irvingjuarez274@gmail.com"><img src="" /></a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </Section>
+                <Section title="About me">
+                    <p>
+                        Frontend engineer with high level of mastery in React js and preprocessors such like Stylus or Sass. I develop prefessional websites using the correct tools, such as Webpack, Babel, etc. 
+                        <br /><br />
+                        I like to create useful web apps that solve real life problems. In my free time I like to develop complex videogames for the web that actually become more complex than most web pages.
+                        <br /><br />
+                        I consider myself a problem solver and you can contact me through an email to start working on your next awesome web page.
+                        <br /><br />
+                    </p>
+                </Section>
 
-                    <Section title="About me">
-                        <p>
-                            Frontend engineer with high level of mastery in React js and preprocessors such like Stylus or Sass. I develop prefessional websites using the correct tools, such as Webpack, Babel, etc. 
-                            <br /><br />
-                            I like to create useful web apps that solve real life problems. In my free time I like to develop complex videogames for the web that actually become more complex than most web pages.
-                            <br /><br />
-                            I consider myself a problem solver and you can contact me through an email to start working on your next awesome web page.
-                        </p>
-                    </Section>
-                </section>
-            )
-        }
+                {this.renderRepoData()}
+            </section>
+        )
     }
 }
 
