d8529657eeab68eb396553a83713b7b857f275b7
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-18 22:40:39 -0500

    There was a problem with Node.js latest version and Webpack. But it is already solved

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const CopyPlugin = require("copy-webpack-plugin")
-const Dotenv = require("dotenv-webpack")
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        publicPath: "/"
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
-            {
-                test: /.html$/,
-                use: [{loader: "html-loader"}]
-            },
-            {
-                test: /.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(svg|png|gif|jpg)/,
-                type: "asset/resource"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html",
-            favicon: "./src/assets/icons/favicon.png"
-        }),
-        new CopyPlugin({
-            patterns: [
-                {from: "./src/assets", to: "./assets"}
-            ]
-        }),
-        new Dotenv()
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /.html$/,
+        use: [{ loader: 'html-loader' }],
+      },
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|png|gif|jpg)/,
+        type: 'asset/resource',
+      },
     ],
-    devServer: {
-        historyApiFallback: true
-    }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './src/assets/icons/favicon.png',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/assets', to: './assets' },
+      ],
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
