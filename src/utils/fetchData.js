af9d1da990c5ede5586688a07ce6cb45240a003c
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-25 23:00:17 -0500

    new service has been created, giving us a cleaner and more understandable code

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,57 +1,5 @@
 import { getApiRequest } from "./getApiRequest";
-
-const getOneCallAPI = (data) => {
-  const { lon, lat } = data.coord;
-  let oneCallAPI = process.env.WeatherOneCallAPI.replace('LAT', lat);
-  oneCallAPI = oneCallAPI.replace('LON', lon);
-
-  return oneCallAPI;
-};
-
-const getData = async (API, component, flag) => {
-
-  try {
-    const response = await fetch(API);
-    const data = await response.json();
-
-    const newAPI = getOneCallAPI(data);
-
-    const newResponse = await fetch(newAPI);
-    const newData = await newResponse.json();
-
-    const superData = {
-      ...data,
-      ...newData,
-    };
-
-    component.setState({
-      loading: false,
-      data: superData,
-    });
-
-  } catch (error) {
-
-    component.setState({
-      loading: false,
-      error,
-    });
-  }
-};
-
-
-
-const requestData = (lat, lon, component, name) => {
-  let API;
-
-  if (name) {
-    API = getApiRequest(name);
-  } else {
-    API = process.env.WeatherAPICoor.replace('LAT', lat);
-    API = API.replace('LON', lon);
-  }
-
-  getData(API, component, true);
-};
+import { newRequestData } from "./newRequestData"
 
 const fetchData = async (component, name) => {
   component.setState({
@@ -61,10 +9,9 @@ const fetchData = async (component, name) => {
 
   /** Here we are assumming the name will be always there */
   const API = getApiRequest(name);
-  const data = await getData(API, component);
+  newRequestData(API, component)
 
-  requestData(data.coord.lat, data.coord.lon, component);
-
+  /**Code I will use later on, because the geolocation feature is going to be added lately */
   // window.navigator.geolocation.getCurrentPosition((position) => {
   //   const lon = position.coords.longitude;
   //   const lat = position.coords.latitude;
