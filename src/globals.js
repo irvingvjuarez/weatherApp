bb496529bd1cef55dc1a74b0d912a82703cf3874
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-25 23:08:34 -0500

    Random cities feature already added

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,13 @@
 export const API_BY_CITY_NAME = "https://api.openweathermap.org/data/2.5/weather?q=NAME&units=metric&appid=" + process.env.WeatherAPIKey
 export const WEATHER_ONE_CALL_API = "https://api.openweathermap.org/data/2.5/onecall?lat=LAT&lon=LON&units=metric&appid=" + process.env.WeatherAPIKey
+
+export const RANDOM_CITIES = [
+  "Mexico City",
+  "Bogota",
+  "Buenos Aires",
+  "London",
+  "Paris",
+  "Washington",
+  "Madrid",
+  "Venice"
+]
