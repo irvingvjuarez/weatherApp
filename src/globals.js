495f9f2926242118433f94d0257acef043850b02
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-26 20:38:40 -0500

    The local storage coordinates feature is fully developed

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,8 @@
-export const API_BY_CITY_NAME = "https://api.openweathermap.org/data/2.5/weather?q=NAME&units=metric&appid=" + process.env.WeatherAPIKey
-export const WEATHER_ONE_CALL_API = "https://api.openweathermap.org/data/2.5/onecall?lat=LAT&lon=LON&units=metric&appid=" + process.env.WeatherAPIKey
+const apiKey = process.env.WeatherAPIKey
+
+export const API_BY_CITY_NAME = "https://api.openweathermap.org/data/2.5/weather?q=NAME&units=metric&appid=" + apiKey
+export const API_BY_COORD = "https://api.openweathermap.org/data/2.5/weather?lat=LAT&lon=LON&units=metric&appid=" + apiKey
+export const WEATHER_ONE_CALL_API = "https://api.openweathermap.org/data/2.5/onecall?lat=LAT&lon=LON&units=metric&appid=" + apiKey
 
 export const RANDOM_CITIES = [
   "Mexico City",
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,4 @@
+import { API_BY_COORD, WEATHER_ONE_CALL_API } from "../globals";
 import { getApiRequest } from "./getApiRequest";
 import { newRequestData } from "./newRequestData"
 
@@ -10,18 +11,37 @@ const fetchData = async (component, name) => {
   const locationCoord = localStorage.getItem("locationCoord")
 
   if(locationCoord){
-    console.log("location coordinates:", locationCoord)
+    const { longitude, latitude } = JSON.parse(locationCoord)
+    const APIs = [
+      API_BY_COORD.replace("LAT", latitude).replace("LON", longitude),
+      WEATHER_ONE_CALL_API.replace("LAT", latitude).replace("LON", longitude)
+    ]
+
+    Promise.all(APIs.map(api => fetch(api)))
+      .then(results => Promise.all(results.map(res => res.json())))
+      .then(datas => {
+        let locationData = {}
+        datas.forEach(data => {
+          locationData = {
+            ...locationData,
+            ...data
+          }
+        })
+        component.setState({
+          loading: false,
+          data: {...locationData}
+        })
+      })
   }else{
     window.navigator.geolocation.getCurrentPosition(position => {
       const { longitude, latitude } = position.coords;
       const coordinates = JSON.stringify({longitude, latitude})
       localStorage.setItem("locationCoord", coordinates)
-    }, () => console.log("No position gotten"));
+    }, () => {
+      const API = getApiRequest(name);
+      newRequestData(API, component)
+    });
   }
-
-  /** Here we are assumming the name will be always there */
-  const API = getApiRequest(name);
-  newRequestData(API, component)
 
 };
 
