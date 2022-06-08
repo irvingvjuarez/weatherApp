03ac4f592000856a435b0ba9e23992ce5c34b00b
Irving Juárez <irvingjuarez274@gmail.com>
2022-06-07 22:10:25 -0500

    Change in the Banner text is already done, but the function of the btn is missing yet

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,19 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { getCurrentLocation } from "./services/getCurrentLocation"
 import "../styles/Banner.css"
+import { RANDOM_CITIES } from "../../globals"
 
 const Banner = ({ component }) => {
-  const handleClick = () => {
+  const [bannerState, setBannerState] = useState({
+    message: "Get the weather of my current location",
+    buttonTitle: "GET NOW"
+  })
 
+  const handleGetCurrentLocation = () => {
     /**Method to ask for the geolocation and get current location weather info */
     getCurrentLocation(component)
   }
 
+  useEffect(() => {
+    const cityName = document.querySelector(".header__cityName").textContent
+
+    const isCityNameInRandomCities = RANDOM_CITIES.find(city => city.name === cityName.trim())
+    if(isCityNameInRandomCities === undefined){
+      setBannerState({
+        message: "Set this location as the default one",
+        buttonTitle: "SET NOW"
+      })
+    }
+
+  }, [])
+
   return(
     <div className="banner">
-      <h2 className="banner__title">Get the weather of my current location</h2>
-      <button className="banner__button" onClick={handleClick}>
-        GET NOW
+      <h2 className="banner__title">{bannerState.message}</h2>
+      <button className="banner__button" onClick={handleGetCurrentLocation}>
+        {bannerState.buttonTitle}
       </button>
     </div>
   )
