9dbda85ccec853ab78c9d080e30f6eb7579f53f6
Irving Juárez <irvingjuarez274@gmail.com>
2022-06-08 01:27:22 -0500

    Modularization of the code, specially in the state

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
-import { getCurrentLocation } from "./services/getCurrentLocation"
 import { RANDOM_CITIES } from "../../globals"
+import { initialState, secondaryState } from "./state"
 
 // css
 import "../styles/Banner.css"
@@ -9,48 +9,20 @@ import "../styles/Banner.css"
 import CloseBtn from "../../assets/icons/cancel.svg"
 
 const Banner = ({ component }) => {
-  const [bannerState, setBannerState] = useState({
-    message: "Get the weather of my current location",
-    buttonTitle: "GET NOW",
-    buttonAction: () => {
-      /**Method to ask for the geolocation and get current location weather info */
-      getCurrentLocation(component)
-    },
-    doneTitle: ""
-  })
+  const [bannerState, setBannerState] = useState(initialState(component))
 
   const handleCloseBanner = () => setBannerState({
-    ...bannerState,
     message: ""
   })
 
   useEffect(() => {
     const cityName = document.querySelector(".header__cityName").textContent
-
     const isCityNameInRandomCities = RANDOM_CITIES.find(city => city.name === cityName.trim())
+
     if(isCityNameInRandomCities === undefined){
-      setBannerState({
-        ...bannerState,
-        message: "Set this location as the default one",
-        buttonTitle: "SET NOW",
-        buttonAction: () => {
-          /**Here the local storage for the current location is settled */
-          const { data: { name, coord } } = component.state
-          const currentLocation = {
-            name,
-            lon: coord.lon,
-            lat: coord.lat
-          }
-
-          localStorage.setItem("currentLocation", JSON.stringify(currentLocation))
-          setBannerState({
-            ...bannerState,
-            doneTitle: "Done!"
-          })
-        }
-      })
+      /**Using the secondary state */
+      setBannerState(secondaryState(bannerState, component, setBannerState))
     }
-
   }, [])
 
   if(bannerState.message) {
